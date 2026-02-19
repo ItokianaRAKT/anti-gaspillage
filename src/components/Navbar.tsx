4e5784ba6484bf6bdb881ec149cac85881a5c577
ItokianaRAKT <itokianarakt@gmail.com>
2026-02-19 08:05:05 +0300

    refactor: modification de navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import { UserRound } from "lucide-react"
-
+import logo from '../assets/logo/logo-light-transparent.png';
 function Navbar() {
   return (
-    <nav className="bg-emerald-800 text-white p-4 flex justify-between w-full fixed top-0 ">
-      <h1 className="font-bold text-x1">Tsinjo</h1>
-      <div className="space-x-4 flex items-center">
+    <nav className="bg-[#2E6F40] text-white p-4 flex justify-between w-full fixed top-0 items-center">
+      <img src={logo} alt="Logo" className="h-16 "/>
+      <div className="space-x-36 flex text-white text-2xl">
         
-          <button>Accueil</button>
+          <button className="">Accueil</button>
+
           <button>Trouver un repas</button>
           <button>Partager</button>
        
-        <UserRound className="w-10 h-10 text-white" />
       </div>
+        <UserRound className="w-10 h-10 text-white" />
     </nav>
   )
 }
