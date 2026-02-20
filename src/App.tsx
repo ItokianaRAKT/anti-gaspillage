0537e04d4d7375c8efee11801049906b7be05cb0
ItokianaRAKT <itokianarakt@gmail.com>
2026-02-20 15:01:44 +0300

    feat: ajout de etapes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/hero"
+import Etapes from "./components/etapes"
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Etapes />
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { UserRound } from "lucide-react"
-import logo from '../assets/logo/logo-light-transparent.png';
+import { ShoppingBasket } from "lucide-react";
+import logo from '../assets/logo/logo-dark-transparent.png';
 function Navbar() {
   return (
-    <nav className="bg-[#2E6F40] text-white p-4 flex justify-between w-full fixed top-0 items-center">
+    <nav className="bg-white text-[#2E6F40] p-4 flex justify-between w-full fixed top-0 items-center h-20 z-10 shadow-md">
       <img src={logo} alt="Logo" className="h-16 "/>
-      <div className="space-x-36 flex text-white text-2xl">
+      <div className="space-x-36 flex text-[#2E6F40] text-3xl font-medium">
         
           <button className="">Accueil</button>
 
@@ -12,9 +13,12 @@ function Navbar() {
           <button>Partager</button>
        
       </div>
-        <UserRound className="w-10 h-10 text-white" />
+      <div className="flex gap-15">
+        <UserRound className="w-10 h-10 text-[#2E6F40]" />
+        <ShoppingBasket className="w-10 h-10 text-[#2E6F40]" />
+      </div>
     </nav>
   )
 }
-
 export default Navbar
+//#FBE9D5]
