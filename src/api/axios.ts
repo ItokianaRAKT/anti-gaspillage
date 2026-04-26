38e9376bd9941334fd562ff5db4f2c34295404e7
Itokiana RAKT <itokianarakt@gmail.com>
2026-04-26 16:20:32 +0300

    .env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth.store";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, //  IMPORTANT
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
 // =====================
